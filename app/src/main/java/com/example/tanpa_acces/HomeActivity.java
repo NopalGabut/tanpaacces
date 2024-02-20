7a27fabc2f3050412551c3d7d941f1b586a842a5
lenovo <akuntugasndr@gmail.com>
2024-02-20 11:08:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tanpa_acces/HomeActivity.java
@@ -0,0 +1,46 @@
+package com.example.tanpa_acces;
+
+import static com.example.tanpa_acces.R.id.bottom_nav;
+import static com.example.tanpa_acces.R.id.frame_bottom;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class HomeActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        bottomNavigation = findViewById(R.id.bottom_nav);
+        bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()){
+                    case R.id.news:
+                        selectedFragment = new NewsFragment();
+                        break;
+                    case R.id.sport:
+                        selectedFragment = new SportFragment();
+                        break;
+                    case R.id.profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_bottom,selectedFragment).commit();
+                return true;
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/tanpa_acces/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.tanpa_acces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                finish();
+            }
+        },2500);
+    }
+}
